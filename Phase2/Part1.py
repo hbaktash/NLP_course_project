735efd51876985cdc9959864f58ee7a70345ca77
danial23mm <danial23mm@gmail.com>
2019-12-20 02:09:00 +0330

    train and test matrix

--- Phase2/Part1.py
+++ Phase2/Part1.py
@@ -3,7 +3,7 @@ from Phase2.prep_tools import *
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 
+train_x,test_x = get_data_np()
+train_y,test_y = get_labels_np()
 
-term_and_posting_list = prepare_data_as_list(train_not_test=True)
-train_x = term_pl_list_to_matrix(term_and_posting_list)
-print(train_x.sum(axis=1))
+print(train_x.shape,train_y.shape,test_x.shape,test_y.shape)
--- Phase2/prep_tools.py
+++ Phase2/prep_tools.py
@@ -1,5 +1,6 @@
 from Phase1 import dictionary, file_handler
 import numpy as np
+import pandas as pd
 LANGUAGE = 1  # English
 
 
@@ -26,3 +27,24 @@ def term_pl_list_to_matrix(term_pl_list: list, docs_count: int = 9000):
             tf = doc_id_tf[1]
             doc_term_matrix[doc_id-1][i] = tf
     return doc_term_matrix
+
+def get_labels_np():
+    "phase2_train.csv"
+    train_y = pd.read_csv("phase2_train.csv").to_numpy()[:, 0]
+    test_y =  pd.read_csv("phase2_test.csv").to_numpy()[:, 0]
+    return train_y,test_y
+
+def get_data_np():
+    x_train =  np.load("x_train.npy")
+    x_test  = np.load("x_test.npy")
+    return x_train,x_test
+
+def save_data_np():
+
+    term_and_posting_list = prepare_data_as_list(train_not_test=True)
+    train_x = term_pl_list_to_matrix(term_and_posting_list)
+    np.save("x_train.npy",train_x)
+
+    term_and_posting_list = prepare_data_as_list(train_not_test=False)
+    test_x = term_pl_list_to_matrix(term_and_posting_list)
+    np.save("x_test.npy", test_x)

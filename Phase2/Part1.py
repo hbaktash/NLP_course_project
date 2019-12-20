11291ff9787a6a77bb0325798c6f95c7040a1aef
Hossein Baktash <hossein.bktsh@gmail.com>
2019-12-20 09:32:02 +0330

    preprocess as matrix and list

--- Phase2/Part1.py
+++ Phase2/Part1.py
@@ -4,6 +4,9 @@ import numpy as np
 np.set_printoptions(threshold=np.inf)
 
 
-term_and_posting_list = prepare_data_as_list(train_not_test=True)
-train_x = term_pl_list_to_matrix(term_and_posting_list)
+train_term_and_posting_list = prepare_data_as_list(train_not_test=True)
+train_x, train_terms = term_pl_list_to_matrix(train_term_and_posting_list)
+test_term_and_posting_list = prepare_data_as_list(train_not_test=False)
+test_x, _ = term_pl_list_to_matrix_for_test_given_train(test_term_and_posting_list, train_terms)
 print(train_x.sum(axis=1))
+print(test_x.sum(axis=1))
--- Phase2/prep_tools.py
+++ Phase2/prep_tools.py
@@ -1,5 +1,6 @@
 from Phase1 import dictionary, file_handler
 import numpy as np
+
 LANGUAGE = 1  # English
 
 
@@ -24,5 +25,33 @@ def term_pl_list_to_matrix(term_pl_list: list, docs_count: int = 9000):
         for doc_id_tf in doc_id_tfs:
             doc_id = doc_id_tf[0]
             tf = doc_id_tf[1]
-            doc_term_matrix[doc_id-1][i] = tf
+            doc_term_matrix[doc_id - 1][i] = tf
+    return doc_term_matrix, term_arr
+
+
+def term_pl_list_to_matrix_for_test_given_train(term_pl_list: list, train_terms: list):
+    term_arr = [a[0] for a in term_pl_list]
+    mapping = generate_term_map(term_arr, train_terms)
+    doc_id_tfs_list = [a[1].to_list() for a in term_pl_list]
+    doc_term_matrix = np.zeros((len(train_terms), len(term_arr)))
+    for i in range(len(doc_id_tfs_list)):
+        doc_id_tfs = doc_id_tfs_list[i]
+        for doc_id_tf in doc_id_tfs:
+            doc_id = doc_id_tf[0]
+            tf = doc_id_tf[1]
+            if mapping[i] != -1:
+                doc_term_matrix[doc_id - 1][mapping[i]] = tf
+            else:
+                pass
     return doc_term_matrix
+
+
+def generate_term_map(small_list: list, big_list: list):
+    ans = []
+    i = 0
+    for s in small_list:
+        if s in big_list:
+            ans.append(big_list.index(s))
+        else:
+            ans.append(-1)
+    return ans
